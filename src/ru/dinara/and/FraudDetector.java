005e21845384331b10a0c1bccabfda0da8b2bb9d
Dinara <ish.dinara@gmail.com>
2025-05-12 20:49:52 +0300

    refactory

--- src/ru/dinara/and/FraudDetector.java
+++ src/ru/dinara/and/FraudDetector.java
@@ -2,22 +2,35 @@ package ru.dinara.and;
 
 public class FraudDetector {
     public boolean isFraud(Transaction transaction) {
+        return isFraudRule1(transaction)
+                || isFraudRule2(transaction)
+                || isFraudRule3(transaction)
+                || isFraudRule4(transaction)
+                || isFraudRule5(transaction);
+
+    }
+
+    private boolean isFraudRule1(Transaction transaction) {
         Trader trader = transaction.getTrader();
-        if (trader.getFullName().equals("Pokemon")) {
-            return true;
-        }
-        if (transaction.getAmount() > 1000000) {
-            return true;
-        }
-        if (trader.getCity().equals("Sydney")) {
-            return true;
-        }
-        if (trader.getCountry().equals("Jamaica")) {
-            return true;
-        }
-        if (trader.getCountry().equals("Germany") && transaction.getAmount() > 1000) {
-            return true;
-        }
-        return false;
+        return trader.getFullName().equals("Pokemon");
+    }
+
+    private boolean isFraudRule2(Transaction transaction) {
+        return transaction.getAmount() > 1000000;
+    }
+
+    private boolean isFraudRule3(Transaction transaction) {
+        Trader trader = transaction.getTrader();
+        return trader.getCity().equals("Sydney");
+    }
+
+    private boolean isFraudRule4(Transaction transaction) {
+        Trader trader = transaction.getTrader();
+        return trader.getCountry().equals("Jamaica");
+    }
+
+    private boolean isFraudRule5(Transaction transaction) {
+        Trader trader = transaction.getTrader();
+        return trader.getCountry().equals("Germany") && transaction.getAmount() > 1000;
     }
 }
