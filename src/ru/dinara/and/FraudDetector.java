91c7c0d4d28c41b6cd768b519efd00c128428c3c
Dinara <ish.dinara@gmail.com>
2025-05-17 21:21:22 +0300

    add class FraudDetectionResult

--- src/ru/dinara/and/FraudDetector.java
+++ src/ru/dinara/and/FraudDetector.java
@@ -1,16 +1,27 @@
 package ru.dinara.and;
 
 public class FraudDetector {
-    public boolean isFraud(Transaction transaction) {
-        FraudRule1 fraudRule1 = new FraudRule1();
-        FraudRule2 fraudRule2 = new FraudRule2();
-        FraudRule3 fraudRule3 = new FraudRule3();
-        FraudRule4 fraudRule4 = new FraudRule4();
-        FraudRule5 fraudRule5 = new FraudRule5();
-        return fraudRule1.isFraud(transaction)
-                || fraudRule2.isFraud(transaction)
-                || fraudRule3.isFraud(transaction)
-                || fraudRule4.isFraud(transaction)
-                || fraudRule5.isFraud(transaction);
+    private FraudRule1 fraudRule1 = new FraudRule1();
+    private FraudRule2 fraudRule2 = new FraudRule2();
+    private FraudRule3 fraudRule3 = new FraudRule3();
+    private FraudRule4 fraudRule4 = new FraudRule4();
+    private FraudRule5 fraudRule5 = new FraudRule5();
+    public FraudDetectionResult isFraud(Transaction transaction) {
+        if (fraudRule1.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule1");
+        }
+        if (fraudRule2.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule2");
+        }
+        if (fraudRule3.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule3");
+        }
+        if (fraudRule4.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule4");
+        }
+        if (fraudRule5.isFraud(transaction)) {
+            return new FraudDetectionResult(true, "FraudRule5");
+        }
+        return new FraudDetectionResult(false, null);
     }
 }
